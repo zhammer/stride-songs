f581959931f8f0edb949115e26f43ae37c7f11a0
zhammer <zachary_hammer@alumni.brown.edu>
2020-08-07 14:36:42 -0400

    Setup app main page

---
+++ native-app/App.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { Text, View, Button, Alert } from "react-native";
+import tailwind from "tailwind-rn";
+
+function range(count: number): number[] {
+  return Array.from(Array(count).keys());
+}
+
+function useInterval(callback: Function, delay: number) {
+  useEffect(() => {
+    let interval = setInterval(callback, delay);
+    return () => clearInterval(interval);
+  }, [callback, delay]);
+}
+
+export default function App() {
+  let [ticks, setTicks] = useState(0);
+  useInterval(() => setTicks((ticks) => ticks + 1), 1e3);
+
+  function handleLoginButtonPressed() {
+    Alert.alert("huzzah!");
+  }
+
+  return (
+    <View style={tailwind("h-full items-center p-12 pt-40")}>
+      <Text style={tailwind("text-gray-700 text-5xl")}>Stride Songs</Text>
+      <Text style={tailwind("text-2xl pt-5")}>
+        {range(5).map((i) => ((ticks + i) % 5 ? "🎵" : "🏃🏽‍♀️"))}
+      </Text>
+      <Text style={tailwind("text-gray-600 text-2xl pt-5")}>
+        Sync your runs to your favorite Spotify songs.
+      </Text>
+      <View style={tailwind("p-12 text-4xl")}>
+        <Button title="Login with Spotify" onPress={handleLoginButtonPressed} />
+      </View>
+    </View>
+  );
+}
