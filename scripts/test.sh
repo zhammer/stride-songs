31b283f1e40869e79db3278f3b58c98d1a5fb2fb
zhammer <zachary_hammer@alumni.brown.edu>
2020-10-09 16:51:34 -0400

    fix .env missing issue

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# i believe it's a bug that docker-compose complains about not being able
+# to find .env. .env is overriden by .env.test in the override yml file.
+# for the sake of CI passing, we create an empty .env file if it doesnt exist.
+touch .env
+
 docker-compose -f docker-compose.yml -f docker-compose.test.yml up
