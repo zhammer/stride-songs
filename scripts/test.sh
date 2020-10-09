2d4bee130ff05abf463e71d00c7f592c9e446f35
zhammer <zachary_hammer@alumni.brown.edu>
2020-10-09 17:10:44 -0400

    github action fixes

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,7 @@
 # for the sake of CI passing, we create an empty .env file if it doesnt exist.
 touch .env
 
+# we need docker version at least 18.03 to use host.docker.internal on linux
+docker -v
+
 docker-compose -f docker-compose.yml -f docker-compose.test.yml up -d
