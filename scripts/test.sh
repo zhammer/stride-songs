f99f25c883d8d63aa22bc566694d11c3b8f2571b
zhammer <zachary_hammer@alumni.brown.edu>
2020-10-09 19:18:03 -0400

    fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,18 +5,4 @@
 # for the sake of CI passing, we create an empty .env file if it doesnt exist.
 touch .env
 
-# if we're running this in github actions, update our env file to support
-# linux docker routing
-# i _do_ think host.docker.internal support has landed in linux, i just don't
-# know how to set it up at the moment
-# https://stackoverflow.com/questions/48546124/what-is-linux-equivalent-of-host-docker-internal/48547074#48547074
-if ! [ -z "$GITHUB_ACTIONS" ]; then
-  echo "running in github actions, updating .env.test"
-  sed -i 's/host.docker.internal/172.17.0.1/' .env.test
-fi
-
-
-# we need docker version at least 18.03 to use host.docker.internal on linux
-docker -v
-
 docker-compose -f docker-compose.yml -f docker-compose.test.yml up -d
