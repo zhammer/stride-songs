38e1b0d8c00e3d9611f7a7bd81c810988205a9ca
zhammer <zachary_hammer@alumni.brown.edu>
2020-10-10 08:13:37 -0400

    add created_at

--- migrations/1602330869783_create_table_public_stride_events/up.sql
+++ migrations/1602330869783_create_table_public_stride_events/up.sql
@@ -3,6 +3,7 @@ CREATE TABLE "public"."stride_events"(
   "user_id" integer NOT NULL,
   "type" text NOT NULL,
   "payload" jsonb NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
   PRIMARY KEY ("id"),
   FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON UPDATE restrict ON DELETE restrict,
   FOREIGN KEY ("type") REFERENCES "public"."stride_event_types"("value") ON UPDATE restrict ON DELETE restrict
